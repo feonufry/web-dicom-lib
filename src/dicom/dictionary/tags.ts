52cf46878e7714a1237567930d2b37a3f2b3710b
Roman Liberov <r.liberov@gmail.com>
2018-07-25 17:55:07 +0300

    Make Tags Dictionary less verbose: Build tag keywords from tag names

--- src/dicom/dictionary/tags.ts
+++ src/dicom/dictionary/tags.ts
@@ -14,14 +14,14 @@ import {Tag} from "../core/tag";
 import {assertNotNull} from "../../helpers/assertions";
 
 class TagDescriptor<TVR extends VR, TValue extends DicomElementValues> implements TagDescriptorInterface<TVR, TValue> {
-    public readonly name: string;
+    public readonly keyword: string;
 
     public constructor(
             public readonly tag: TagInterface,
             public readonly vr: TVR,
-            public readonly keyword: string,
-            name?: string) {
-        this.name = name || keyword;
+            public readonly name: string,
+            keyword?: string) {
+        this.keyword = keyword || buildTagKeyword(name);
     }
 
     public matches(element: DicomElement): element is DicomElement<TVR, TValue> {
@@ -37,180 +37,123 @@ export const Tags: TagsDictionaryIndexable = {
     find,
     FileMetaInformationGroupLength: new TagDescriptor<"UL", number[]>(
         new Tag(0x0002, 0x0000),
-        "UL",
-        "FileMetaInformationGroupLength",
-        "File Meta Information Group Length",
+        "UL", "File Meta Information Group Length",
     ),
     FileMetaInformationVersion: new TagDescriptor<"OB", Uint8Array>(
         new Tag(0x0002, 0x0001),
-        "OB",
-        "FileMetaInformationVersion",
-        "File Meta Information Version",
+        "OB", "File Meta Information Version",
     ),
     MediaStorageSOPClassUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0002, 0x0002),
-        "UI",
-        "MediaStorageSOPClassUID",
-        "Media Storage SOP Class UID",
+        "UI", "Media Storage SOP Class UID",
     ),
     MediaStorageSOPInstanceUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0002, 0x0003),
-        "UI",
-        "MediaStorageSOPInstanceUID",
-        "Media Storage SOP Instance UID",
+        "UI", "Media Storage SOP Instance UID",
     ),
     TransferSyntaxUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0002, 0x0010),
-        "UI",
-        "TransferSyntaxUID",
-        "Transfer Syntax UID",
+        "UI", "Transfer Syntax UID",
     ),
     ImplementationClassUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0002, 0x0012),
-        "UI",
-        "ImplementationClassUID",
-        "Implementation Class UID",
+        "UI", "Implementation Class UID",
     ),
     ImplementationVersionName: new TagDescriptor<"SH", string[]>(
         new Tag(0x0002, 0x0013),
-        "SH",
-        "ImplementationVersionName",
-        "Implementation Version Name",
+        "SH", "Implementation Version Name",
     ),
     SourceApplicationEntityTitle: new TagDescriptor<"AE", string[]>(
         new Tag(0x0002, 0x0016),
-        "AE",
-        "SourceApplicationEntityTitle",
-        "Source Application Entity Title",
+        "AE", "Source Application Entity Title",
     ),
     SendingApplicationEntityTitle: new TagDescriptor<"AE", string[]>(
         new Tag(0x0002, 0x0017),
-        "AE",
-        "SendingApplicationEntityTitle",
-        "Sending Application Entity Title",
+        "AE", "Sending Application Entity Title",
     ),
     ReceivingApplicationEntityTitle: new TagDescriptor<"AE", string[]>(
         new Tag(0x0002, 0x0018),
-        "AE",
-        "ReceivingApplicationEntityTitle",
-        "Receiving Application Entity Title",
+        "AE", "Receiving Application Entity Title",
     ),
     PrivateInformationCreatorUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0002, 0x0100),
-        "UI",
-        "PrivateInformationCreatorUID",
-        "Private Information Creator UID",
+        "UI", "Private Information Creator UID",
     ),
     PrivateInformation: new TagDescriptor<"OB", Uint8Array>(
         new Tag(0x0002, 0x0102),
-        "OB",
-        "PrivateInformation",
-        "Private Information",
+        "OB", "Private Information",
     ),
     SpecificCharacterSet: new TagDescriptor<"CS", string[]>(
         new Tag(0x0008, 0x0005),
-        "CS",
-        "SpecificCharacterSet",
-        "Specific Character Set",
+        "CS", "Specific Character Set",
     ),
     ImageType: new TagDescriptor<"CS", string[]>(
         new Tag(0x0008, 0x0008),
-        "CS",
-        "ImageType",
-        "Image Type",
+        "CS", "Image Type",
     ),
     SOPClassUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0008, 0x0016),
-        "UI",
-        "SOPClassUID",
-        "SOP Class UID",
+        "UI", "SOP Class UID",
     ),
     SOPInstanceUID: new TagDescriptor<"UI", string[]>(
         new Tag(0x0008, 0x0018),
-        "UI",
-        "SOPInstanceUID",
-        "SOP Instance UID",
+        "UI", "SOP Instance UID",
     ),
     StudyDate: new TagDescriptor<"DA", string[]>(
         new Tag(0x0008, 0x0020),
-        "DA",
-        "StudyDate",
-        "Study Date",
+        "DA", "Study Date",
     ),
     SeriesDate: new TagDescriptor<"DA", string[]>(
         new Tag(0x0008, 0x0021),
-        "DA",
-        "SeriesDate",
-        "Series Date",
+        "DA", "Series Date",
     ),
     AcquisitionDate: new TagDescriptor<"DA", string[]>(
         new Tag(0x0008, 0x0022),
-        "DA",
-        "AcquisitionDate",
-        "Acquisition Date",
+        "DA", "Acquisition Date",
     ),
     StudyTime: new TagDescriptor<"TM", string[]>(
         new Tag(0x0008, 0x0030),
-        "TM",
-        "StudyTime",
-        "Study Time",
+        "TM", "Study Time",
     ),
     SeriesTime: new TagDescriptor<"TM", string[]>(
         new Tag(0x0008, 0x0031),
-        "TM",
-        "SeriesTime",
-        "Series Time",
+        "TM", "Series Time",
     ),
     AcquisitionTime: new TagDescriptor<"TM", string[]>(
         new Tag(0x0008, 0x0032),
-        "TM",
-        "AcquisitionTime",
-        "Acquisition Time",
+        "TM", "Acquisition Time",
     ),
     AccessionNumber: new TagDescriptor<"SH", string[]>(
         new Tag(0x0008, 0x0050),
-        "SH",
-        "AccessionNumber",
-        "Accession Number",
+        "SH", "Accession Number",
     ),
     Modality: new TagDescriptor<"CS", string[]>(
         new Tag(0x0008, 0x0060),
-        "CS",
-        "Modality",
+        "CS", "Modality",
     ),
     Manufacturer: new TagDescriptor<"LO", string[]>(
         new Tag(0x0008, 0x0070),
-        "LO",
-        "Manufacturer",
+        "LO", "Manufacturer",
     ),
     StudyDescription: new TagDescriptor<"LO", string[]>(
         new Tag(0x0008, 0x1030),
-        "LO",
-        "StudyDescription",
-        "Study Description",
+        "LO", "Study Description",
     ),
     SeriesDescription: new TagDescriptor<"LO", string[]>(
         new Tag(0x0008, 0x103E),
-        "LO",
-        "SeriesDescription",
-        "Series Description",
+        "LO", "Series Description",
     ),
     Item: new TagDescriptor<undefined, undefined>(
         new Tag(0xFFFE, 0xE000),
-        undefined,
-        "Item",
+        undefined, "Item",
     ),
     ItemDelimitationItem: new TagDescriptor<undefined, undefined>(
         new Tag(0xFFFE, 0xE00D),
-        undefined,
-        "ItemDelimitationItem",
-        "Item Delimitation Item",
+        undefined,"Item Delimitation Item",
     ),
     SequenceDelimitationItem: new TagDescriptor<undefined, undefined>(
         new Tag(0xFFFE, 0xE0DD),
-        undefined,
-        "SequenceDelimitationItem",
-        "Sequence Delimitation Item",
+        undefined,"Sequence Delimitation Item",
     ),
 };
 
@@ -227,4 +170,8 @@ function find(tag: Tag | number): TagDescriptorInterface | null {
     assertNotNull(tag, "tag");
     const tagNumber = typeof tag === "number" ? tag : tag.toNumber();
     return TagIndex.get(tagNumber) || null;
+}
+
+function buildTagKeyword(tagName: string): string {
+    return tagName.replace(" ", "");
 }
